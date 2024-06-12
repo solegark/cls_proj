11fe96fc647b9c8d96701ceb100b44ff4a96fb90
solegark <77381874+solegark@users.noreply.github.com>
2024-06-12 16:58:20 +0300

    Update run_dock.sh

--- run_dock.sh
+++ run_dock.sh
@@ -1,4 +1,9 @@
-mkdir bin
-s3cmd get -r s3://otus-cls/ bin
+#!/bin/bash
+
+if [! -d "bin"]; then
+	mkdir bin
+	s3cmd get -r s3://otus-cls/ bin
+fi
+
 docker build -t cls_proj:latest .
 docker run -t -dp 5000:8080 cls_proj
