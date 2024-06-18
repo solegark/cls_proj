7431150a972c551e364250044590e401ad79f8df
solegark <77381874+solegark@users.noreply.github.com>
2024-06-18 18:43:29 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,5 +6,6 @@ if [ ! -d "$dr" ]; then
 	mkdir -p bin
 	s3cmd get -r s3://otus-cls/ bin
 fi
-docker build -t cls_proj:latest .
+docker build -t cr.yandex/crpn0ichd5bsrptua8ae/cls-img:latest .
+docker push
 # docker run -t -dp 8080:5000 cls_proj		# запускаем контейнер в случае реализации в docker
